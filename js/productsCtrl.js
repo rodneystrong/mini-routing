0c92e132844e085eb67a3708c6a530543d5cb8bb
aleeex <akang2@gmail.com>
2016-07-18 13:14:29 -0600

    coded condition to check for 'shoes' or 'socks'

--- js/productsCtrl.js
+++ js/productsCtrl.js
@@ -1,5 +1,14 @@
 angular
   .module('miniRouting')
-  .controller('productsCtrl', function() {
-    
+  .controller('productsCtrl', function($stateParams, productService) {
+    //in the code below, the 'id' is arbitrary.
+    //it coincides with the id in our index.html
+    if($stateParams.id === "shoes") {
+      //the .productData is just an arbitrary word we created
+      //right now to hold the value of .shoeData
+      $scope.productData = productService.shoeData;
+    }
+    else if($stateParams.id === "socks") {
+      $scope.productData = productService.sockData;
+    }
   });
