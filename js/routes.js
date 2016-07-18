8ef4580958b5233628a34b85e6f2e330a476bd4b
aleeex <akang2@gmail.com>
2016-07-18 12:17:37 -0600

    adding the rest of the .state methods to route.js and included the urlRouterProvider.otherwise to catch all incorrect URLs

--- js/routes.js
+++ js/routes.js
@@ -6,5 +6,19 @@ angular
         url:'/',
         templateUrl: 'home.html',
         controller: 'homeCtrl'
+      })
+      .state('settings',{
+        url: '/settings',
+        templateUrl: 'settings.html',
+        controller: 'settingsCtrl'
+      })
+      .state('products', {
+        url: '/products/:id',
+        templateUrl: 'products.html',
+        controller: 'productsCtrl'
       });
+      //This is a catch all for our routes.
+      //"If you can't find the route, send back to home".
+      //Kinda' like a global 404
+      $urlRouterProvider.otherwise("/");
   });
