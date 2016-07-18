42118eb7521dbbb40a5a2ddcc78665390d7c6808
aleeex <akang2@gmail.com>
2016-07-18 12:39:10 -0600

    added <a> to products <li> in index.html

--- js/routes.js
+++ js/routes.js
@@ -7,15 +7,15 @@ angular
         templateUrl: '/views/home.html',
         controller: 'homeCtrl'
       })
-      .state('settings',{
-        url: '/settings',
-        templateUrl: '/views/settings.html',
-        controller: 'settingsCtrl'
-      })
       .state('products', {
         url: '/products/:id',
         templateUrl: '/views/products.html',
         controller: 'productsCtrl'
+      })
+      .state('settings',{
+        url: '/settings',
+        templateUrl: '/views/settings.html',
+        controller: 'settingsCtrl'
       });
       //This is a catch all for our routes.
       //"If you can't find the route, send back to home".
